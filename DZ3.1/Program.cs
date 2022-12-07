b62211cfef1fea5e9e47be6cf8b3f5bb0f4dfd3f
zhagynchik <zhagynchik@mail.ru>
2022-12-07 20:19:12 +0300

    First commit

---
+++ DZ3.1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да  */
+
+
+Console.WriteLine($"Введите пятизначное число: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+if (number/10000%10 == number%10 && number/1000%10 == number/10%10)
+{
+     Console.WriteLine("Да, палиндром");
+}
+else
+{
+     Console.WriteLine("Не палиндром");
+}
+
+
